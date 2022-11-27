a9bdf9bc093e5f1423952f456a9f3372ab824d26
Sunny Prakash <sunnypr12@outlook.com>
2022-11-27 20:16:08 +0100

    fix: base property in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  base: "/solid-gh-pages/",
+  base: '/solid-gh-pages/',
   plugins: [solidPlugin()],
   server: {
     port: 3000,
